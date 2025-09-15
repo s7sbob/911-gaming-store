1ede631376ada1b8d3f60ffac726c985a46f2be6
Hussien Tarek <shs.bob.hussien@gmail.com>
2025-09-16 02:39:23 +0300

    first commit

---
+++ 911-gaming-store/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="min-h-screen flex bg-black text-white items-center justify-center text-2xl font-bold text-center">
+			Hello Vite + React + TailwindCSS!
+		</div>
+	);
+}
+
+export default App;
